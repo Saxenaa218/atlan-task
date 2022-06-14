7bd029213301fc1e5103facb3f52d92364c52af7
Abhishek <saxenaa218@gmail.com>
2022-06-15 01:52:30 +0530

    fix: add code spliting in routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import PeekDialog from "./components/PeekDialog";
+import Loader from "./components/Loader";
 
 import "./App.scss";
+
+const Home = lazy(() => import("./pages/Home"));
+const PeekDialog = lazy(() => import("./components/PeekDialog"));
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/peek" element={<PeekDialog />}></Route>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Home />
+            </Suspense>
+          }
+        ></Route>
+        <Route
+          path="/peek"
+          element={
+            <Suspense fallback={<Loader />}>
+              <PeekDialog />
+            </Suspense>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
